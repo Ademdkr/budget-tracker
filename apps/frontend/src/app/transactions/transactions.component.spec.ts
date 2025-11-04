f8a4992077a41e3654712e8d57cf315444484de5
Adem <adem.dokur@outlook.de>
2025-11-04 17:26:30 +0100

    fix: add provideAnimations to TransactionsComponent test
    Add provideAnimations to fix Angular Material test error:
    NG05105: Unexpected synthetic listener @arrowPosition.start found

--- apps/frontend/src/app/transactions/transactions.component.spec.ts
+++ apps/frontend/src/app/transactions/transactions.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { TransactionsComponent } from './transactions.component';
 
@@ -10,9 +11,8 @@ describe('TransactionsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TransactionsComponent],
-      providers: [provideHttpClient()]
-    })
-    .compileComponents();
+      providers: [provideHttpClient(), provideAnimations()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TransactionsComponent);
     component = fixture.componentInstance;
